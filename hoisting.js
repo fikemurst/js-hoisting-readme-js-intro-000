383b8e49cb6e33b7465d65b1aff1db8b574551a9
michael furst <michael@furst.us>
2018-05-22 23:00:12 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -19,12 +19,11 @@ function crazy() {
 
 function sayMyName() {
   // fix the code in here:
-  
-
-  sayMy();
-
+  var name = "Cricky";
+var name = "Kristin";
   function sayMy() {
     console.log(name);
-    var name = "Kristin";
+    name = "Kristin"
   }
 }
+sayMy();
